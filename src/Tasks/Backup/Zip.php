3d281ab06fa28f638a4e518cd770fdf1f4976a1f
jignesh sanghani <jigneshsanghani6@gmail.com>
2019-12-02 19:23:42 +0530

    Change zip folder path

--- src/Tasks/Backup/Zip.php
+++ src/Tasks/Backup/Zip.php
@@ -101,7 +101,7 @@ class Zip
 
         foreach ($files as $file) {
             if (file_exists($file)) {
-                $this->zipFile->addFile($file, ltrim($nameInZip, DIRECTORY_SEPARATOR)).PHP_EOL;
+                $this->zipFile->addFile($file, str_replace( base_path(),'' , ltrim(str_replace( base_path(),'' , ltrim($nameInZip, DIRECTORY_SEPARATOR)),DIRECTORY_SEPARATOR))).PHP_EOL;
             }
             $this->fileCount++;
         }
